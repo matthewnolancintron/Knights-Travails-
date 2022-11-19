0875f01b85965dd8e5f31371d05298befd0c511e
Matthew Shane Nolan-Cintron <matthewnolancintron@gmail.com>
2022-11-19 16:40:10 -0500

    implemented KnightFactory a factory function that generates knight objects not sure what props a knight should have or how I should use the knight Object yet

--- KnightAndGameBoard.js
+++ KnightAndGameBoard.js
@@ -1,15 +1,22 @@
 function GameBoardFactory(){
+    //y is used for the rows
     let y = 0;
     return {
-        gameBoard:Array.from(Array(64).keys(),x=>[x%8===0?++y-1:y-1,x%8]);
+        //
+        gameBoard:Array.from(Array(64).keys(),x=>[x%8===0?++y-1:y-1,x%8]),
     }
 }
 
         
 function KnightFactory(){
-    
+    return {
+        //
+        possibleMoves:[]
+    }
 }
 
 
 let GameBoardObject = GameBoardFactory();
+let KnightObject = KnightFactory();
+console.log(KnightObject)
 console.log(GameBoardObject.gameBoard);
